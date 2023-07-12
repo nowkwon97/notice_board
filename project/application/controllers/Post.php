c2c7471d253cbcffc993a6ba6a3951e7959b1146
leejaekwon <nowkwon97@gmail.com>
2023-07-12 09:29:19 +0900

    UPDATE controllers에 Post.php 추가 및 기능별 public function 구분

---
+++ project/application/controllers/Post.php
@@ -0,0 +1,23 @@
+<?php
+  class Posts extends CI_Controller {
+    public function index() {
+      // 게시글 목록 조회 기능 구현
+    }
+
+    public function view($id) {
+      // 게시글 상세 조회 기능 구현
+    }
+
+    public function create() {
+      // 게시글 작성 기능 구현
+    }
+
+    public function edit($id) {
+      // 게시글 수정 기능 구현
+    }
+
+    public function delete($id) {
+      // 게시글 삭제 기능 구현
+    }
+  }
+?>
