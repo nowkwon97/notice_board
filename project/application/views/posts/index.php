5dddbf28b3c09fe78d87f34d465bec1121859de9
leejaekwon <nowkwon97@gmail.com>
2023-07-12 14:35:18 +0900

    UPDATE DB의 데이터를 조회하는 로직 완성

--- project/application/views/posts/index.php
+++ project/application/views/posts/index.php
@@ -13,7 +13,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <h2>게시글 목록</h2>
   
   <?php
-    print_r($posts);
+    print_r($posts[0]);
+    echo "<br>";
+    echo "<br>";
+    // echo "제목: " . $posts[0]['title'] . "<br>";
+    // echo "작성일: " . $posts[0]['created_at'] . "<br>";
+    // echo "내용: " . $posts[0]['content'] . "<br>";
   ?>
+
+
+  <!-- posts 배열의 모든 요소를 순회하며 반복 -->
+  <?php foreach($posts as $post) : ?>
+    <h3>
+      <?php echo "제목: " . $post['title'] . "<br>"; ?>
+    </h3>
+    <p>
+      <?php echo "작성일: " . $post['created_at'] . "<br>"; ?>
+    </p>
+    <p>
+      <?php echo "내용: " . $post['content'] . "<br>"; ?>
+    </p>
+  <?php endforeach?>
+
 </body>
 </html>
