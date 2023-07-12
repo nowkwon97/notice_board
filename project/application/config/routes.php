2bb1da2813f8678099742a0b008c7548c987e639
leejaekwon <nowkwon97@gmail.com>
2023-07-12 10:20:26 +0900

    UPDATE 라우팅 설정

--- project/application/config/routes.php
+++ project/application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['posts'] = 'posts/index'; // Posts 컨트롤러의 index 메서드 호출하도록 설정
