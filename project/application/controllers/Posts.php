7c4ba407c716c4cf9177756f7086ada68cbb266b
leejaekwon <nowkwon97@gmail.com>
2023-07-13 15:58:33 +0900

    UPDATE edit 기능 구현 완료

--- project/application/controllers/Posts.php
+++ project/application/controllers/Posts.php
@@ -4,6 +4,8 @@
     public function index() {
       //* 게시글 목록 조회 기능 구현
 
+      // $this->load->helper('url');
+
       // mysqli가 아닌 CI 내장 DB 라이브러리를 이용하여 DB를 연결하고 테이블 데이터를 받아오는 로직으로 수정 필요.
       // DB 정보 불러오기
       //? APPPATH는 CI 프레임워크에서 애플리케이션 디렉토리의 경로를 나타내는 상수이다.
@@ -95,18 +97,52 @@
       
       $this->load->view('posts/create');
     }
-
+    //! edit 부분에서 오류 발생 중.
+    //! $id인수를 받아오지 못하는 오류로 보임..왜..?
+    //! 게시물 수정 누를 시 url에서 id값이 사라져서 해당 오류가 나타나는 것 확인.
     public function edit($id) {
       // 게시글 수정 기능 구현
-      echo $id;
+      $this->load->library('form_validation');
+      $this->load->helper('url');
+
+      // echo $id;
+      // echo "<br>";
 
       // DB 연결
-      $this->db->database();
+      $this->load->database();
 
-      // 게시글 수정 쿼리
-      $query = $this->db->
+      //? 요청 시의 title, content를 변수에 담아 수정 쿼리에 넣어주는 작업 필요.
+      //? 기존에 작성되어 있던 title, content를 view에 전달하는 로직 필요. view로 전달해주는 변수를 하나 더 만들어야 하나? -> 배열을 이용해 해결
+      
+      // 게시글 조회 쿼리
+      $query = $this->db->query("SELECT * FROM posts WHERE id = $id");
+      $data['posts'] = $query->result_array();
+      // print_r($data['posts'][0]['content']);
+      //* 현재의 title, content를 edit.php으로 보내기 위해 변수 선언 및 할당
+      $title = $data['posts'][0]['title'];
+      $content = $data['posts'][0]['content'];
 
-      $this->load->view('posts/edit');
+      $this->form_validation->set_rules('title', '제목', 'required');
+      $this->form_validation->set_rules('content', '내용', 'required');
+      if ($this->form_validation->run() === FALSE){
+        // 유효성 검사 실패 시, 다시 작성 폼 표시
+        $this->load->view('posts/edit', array('data'=> $data, 'title'=> $title, 'content'=>$content));
+        return;
+      } else {
+        $postData = array(
+          'title' => $this->input->post('title'),
+          'content' => $this->input->post('content'),
+        );
+        // 해당 ID에 해당하는 게시물을 수정
+        $this->db->where('id', $id);
+        $this->db->update('posts', $postData);
+
+        redirect('posts');
+      }
+
+
+      //* 두 개 이상의 변수를 보내기 위해 배열을 이용해 전송
+      // $this->load->view('posts/edit', array('data'=> $data, 'title'=> $title, 'content'=>$content));
     }
 
     public function delete($id) {
--- project/application/views/posts/edit.php
+++ project/application/views/posts/edit.php
@@ -1,2 +1,29 @@
 <!-- 게시물 수정 -->
-<h2>게시물 수정</h2>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>게시물 수정</title>
+</head>
+<body>
+  <h2>게시물 수정</h2>
+  <?php
+    print_r($data['posts'][0]['id']);
+  ?>
+  <?php echo validation_errors(); ?>
+	<?php echo form_open('posts/edit/' . $data['posts'][0]['id']) ?>
+    <div>
+      <label for="title">제목</label>
+      <input type="text" name="title" value="<?php echo $title ?>" required>
+    </div>
+    <div>
+      <label for="content">내용</label>
+      <textarea name="content" rows="5" required><?php echo $content ?></textarea>
+    </div>
+    <input type="submit" name="submit" value="게시물 수정" />
+  </form>
+</body>
+</html>
