b5d2f307127690abb8020b338f1d40719e9638ae
leejaekwon <nowkwon97@gmail.com>
2023-07-17 10:51:03 +0900

    UPDATE 주석 추가

--- project/application/controllers/Create.php
+++ project/application/controllers/Create.php
@@ -12,25 +12,32 @@
 
     public function index() {
       //* 게시글 작성 기능 구현
-      // 폼 유효성 검사
+      /**
+       * ? 1. 폼 유효성 검사
+       * ? 2. 폼 유효성 검사 결과에 따라 처리
+       * ?  2.1 폼 유효성 검사 실패 시, 다시 작성 폼 표시
+       * ?  2.2 폼 유효성 검사 성공 시, 데이터 저장
+       * ? 3. 모델에서 createPost()호출하여 게시글 생성
+       * ? 4. 생성 후 게시판으로 리디렉션
+       */
+      // 1. 폼 유효성 검사
       $this->form_validation->set_rules('title', '제목', 'required');
       $this->form_validation->set_rules('content', '내용', 'required');
-
+      // 2. 폼 유효성 검사 결과에 따라 처리
       if ($this->form_validation->run()===FALSE) {
-        // 유효성 검사 실패 시, 다시 작성 폼 표시
+        // 2.1 유효성 검사 실패 시, 다시 작성 폼 표시
         $this->load->view('posts/create');
         return;
       } else {
-        // 유효성 검사 통과 시, 데이터 저장
-
+        // 2.2 유효성 검사 통과 시, 데이터 저장
         $data = array(
           'title' => $this->input->post('title'),
           'content' => $this->input->post('content'),
           'created_at' => date('Y-m-d H:i:s', time())
         );
-        // Create_model 에서 createPost()함수 호출
+        // 3. 모델에서 createPost()호출하여 게시글 생성
         $this->Create_model->createPost($data);
-        // 작성 후 다시 리디렉션
+        // 4. 생성 후 게시판으로 리디렉션
         redirect('posts');
       }
     }
