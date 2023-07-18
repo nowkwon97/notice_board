d78cf02a81d592db247bf377311897daabcc22da
leejaekwon <nowkwon97@gmail.com>
2023-07-18 13:42:37 +0900

    UPDATAE 로그인 한 계정의 글만 삭제하도록 변경->버그 발견

--- project/application/controllers/Delete.php
+++ project/application/controllers/Delete.php
@@ -4,18 +4,31 @@
       parent::__construct();
       $this->load->model('Delete_model');
       $this->load->helper('url');
+      $this->load->library('session');
     }
 
     public function index($id) {
       //* 게시글 삭제 기능 구현
       /**
+       * todo 로그인한 계정의 id에 따라 posts의 user_id를 판별하고 본인 계정만 삭제가 가능하도록 하는 기능 구현 필요
        * ? 1. 모델에서 deletePost()호출하여 게시글 삭제
        */
-      
-      // 1. 모델에서 deletePost() 호출하여 게시글 삭제
-      $this->Delete_model->deletePost($id);
+      $username = $this->session->userdata('username');
+      $user_id = $this->Delete_model->getUserIdByUsername($username);
+      $query = $this->Delete_model->getIdByUserId($user_id);
+      $queryId = $query->row()->id;
+      echo $queryId;
+      //! posts테이블의 user_id와 users테이블의 id가 일치할 시 로 바꿔야 함.
+      if($queryId===$id) {
+        // $username의 id($user_id)가 id와 일치할 시
 
-      $this->load->view('posts/delete');
+        // 1. 모델에서 deletePost() 호출하여 게시글 삭제
+        $this->Delete_model->deletePost($id);
+        $this->load->view('posts/delete');
+      } else {
+        // $username의 id($user_id)가 id와 일치하지 않을 시
+        echo "<h3>권한이 없습니다.</h3>";
+      }
     }
   }
 ?>
--- project/application/models/Delete_model.php
+++ project/application/models/Delete_model.php
@@ -9,5 +9,23 @@
       // posts 테이블에서 id=$id인 데이터 삭제
       $this->db->delete('posts', array('id'=>$id));
     }
+
+    // username의 id를 반환하는 함수
+    public function getUserIdByUsername($username) {
+      $query = $this->db->select('id')->from('users')->where('username', $username)->get();
+      // 쿼리 결과가 존재 할 시
+      if ($query->num_rows()>0) {
+        // 쿼리 결과의 row를 담는 변수
+        $row = $query->row();
+        // 직접 접근하여 결과값의 id를 반환
+        return $row->id;
+      }
+    }
+
+    // posts 테이블에서 user_id가 $user_id인 id를 선택 하는 함수
+    public function getIdByUserId($user_id) {
+      $query = $this->db->query("SELECT id FROM posts WHERE user_id = $user_id");
+      return $query;
+    }
   }
 ?>
